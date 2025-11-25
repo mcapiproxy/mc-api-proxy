271a0fcb54863c1fb7ac496bdcdf99585812e9c0
aiwao <233898056+aiwao@users.noreply.github.com>
2025-11-25 14:41:01 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+
+app.use(
+    cors({
+        origin: ["*"]
+    })
+);
 
 app.all("/:endpoint{.+}", async (c) => {
   const endpoint = await c.req.param("endpoint")
